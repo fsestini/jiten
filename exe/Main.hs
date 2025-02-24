7cc3175bf84b3527b0c710fa05d68b13d7c0497f
Filippo Sestini <sestini.filippo@gmail.com>
2025-02-24 22:57:37 +0000

    feat: add empty Jiten.Server module

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,8 +1,6 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import Jiten.Server (runServer)
 
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+main = runServer
---
+++ src/Jiten/Server.hs
@@ -0,0 +1,4 @@
+module Jiten.Server where
+
+runServer :: IO ()
+runServer = pure ()
