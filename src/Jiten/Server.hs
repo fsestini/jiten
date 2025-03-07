e3524e4ee53387e0b2585abaeeb9c6c31fd5fad4
Filippo Sestini <filippo@imandra.ai>
2025-03-07 00:04:44 +0000

    tidy: remove unused dependency 'network'

--- src/Jiten/Server.hs
+++ src/Jiten/Server.hs
@@ -3,9 +3,6 @@ module Jiten.Server where
 import qualified Data.Aeson as A
 import Data.Text (Text)
 import Data.Text.Encoding (decodeUtf8, encodeUtf8)
-import Network.Socket (Family (AF_UNIX), SockAddr (SockAddrUnix), Socket, SocketType (Stream))
-import qualified Network.Socket as Socket
-import qualified Network.Socket.ByteString as Socket (recv, sendAll)
 import Web.Scotty (scotty)
 import qualified Web.Scotty as Scotty
 
