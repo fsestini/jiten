7ba1aab0931845c4f3d6dca7367b4eef12c7e9cd
Filippo Sestini <filippo@imandra.ai>
2025-03-07 19:31:51 +0000

    tidy: remove unused imports

--- src/Jiten/Server.hs
+++ src/Jiten/Server.hs
@@ -1,8 +1,5 @@
 module Jiten.Server where
 
-import qualified Data.Aeson as A
-import Data.Text (Text)
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import Web.Scotty (scotty)
 import qualified Web.Scotty as Scotty
 
