a98ea9d8804de3e08fd88b93151db367748648b5
Filippo Sestini <filippo@imandra.ai>
2025-03-08 21:04:08 +0000

    wip: add test module YomichanSpec

---
+++ test/Jiten/YomichanSpec.hs
@@ -0,0 +1,28 @@
+module Jiten.YomichanSpec where
+
+import qualified Database.SQLite.Simple as Sql
+import qualified Jiten.Database as Db
+import qualified Jiten.Yomichan.Core as Core
+import qualified Jiten.Yomichan.Dictionary as Dict
+import qualified Jiten.Yomichan.Search as Search
+import Test.Hspec (Spec, around, describe, it, shouldBe)
+
+spec :: Spec
+spec =
+  around withYomiCtx $ do
+    describe "on test dictionary 'valid-dictionary1.zip'" $ do
+      describe "findTerms" $ do
+        it "correctly returns results for '打ち込む' on simple mode" $ \ctx -> do
+          -- let text = "打ち込む"
+          -- result <- Search.findTerms ctx Search.Simple text
+          -- result `shouldBe` ""
+          "" `shouldBe` ""
+  where
+    dictPath = "./test/valid-dictionary1.zip" :: FilePath
+    withYomiCtx f =
+      Sql.withConnection ":memory:" $ \conn -> do
+        Db.initDatabase conn
+        dict <- Dict.openArchiveFile dictPath
+        Db.insertDictionary conn dict
+        let getDictIds = map fst <$> Db.getDictionaries conn
+        Core.withYomitan conn getDictIds f
--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import qualified Jiten.DatabaseSpec
+import qualified Jiten.YomichanSpec
 import Test.Hspec (Spec, describe, hspec)
 
 main :: IO ()
@@ -9,3 +10,4 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "Jiten.Database" Jiten.DatabaseSpec.spec
+  describe "Jiten.Yomichan" Jiten.YomichanSpec.spec
