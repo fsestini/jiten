ff372b734d98d44a5f57bc7105095100e2e02715
Filippo Sestini <sestini.filippo@gmail.com>
2025-03-08 22:49:08 +0000

    fix(js): arguments passed to db functions

--- js/yomi.js
+++ js/yomi.js
@@ -2,19 +2,15 @@ import { Translator } from './yomitan/js/language/translator.js'
 
 class DictionaryDatabase {
   findTermsBulk(termList, dictionaries, matchType) {
-    const args = { termList: termList, dictionaries: dictionaries, matchType: matchType }
-    console.log('findTermsBulk.args: ', args)
-    const argsJson = JSON.stringify(args)
-    const resultJson = _findTermsBulk(argsJson)
+    // TODO: use other arguments
+    const resultJson = _findTermsBulk(JSON.stringify(termList))
     const result = JSON.parse(resultJson)
-    console.log('findTermsBulk.result: ', result)
     return result
   }
 
   findTermMetaBulk(termList, dictionaries) {
-    const args = { termList: termList, dictionaries: dictionaries }
-    const argsJson = JSON.stringify(args)
-    const resultJson = _findTermMetaBulk(argsJson)
+    // TODO: use other arguments
+    const resultJson = _findTermMetaBulk(JSON.stringify(termList))
     const result = JSON.parse(resultJson)
     return result
   }
