e49f82e8694839fb615df62f095626a9bdc40421
Filippo Sestini <sestini.filippo@gmail.com>
2025-03-08 23:09:46 +0000

    fix: database test spec

--- test/Jiten/DatabaseSpec.hs
+++ test/Jiten/DatabaseSpec.hs
@@ -16,10 +16,10 @@ spec =
       describe "findTermsBulk" $ do
         it "returns terms" $ \conn -> do
           results <- Db.findTermsBulk conn ["打つ"] [1]
-          let res1 = Db.TermResult 3 "打つ" "うつ" "term" "[\"utsu definition 1\",\"utsu definition 2\"]" "vt" "P E1" 10 "Test Dictionary" 0
-          let res2 = Db.TermResult 4 "打つ" "うつ" "term" "[\"utsu definition 3\",\"utsu definition 4\"]" "vt" "P E2" 1 "Test Dictionary" 1
-          let res3 = Db.TermResult 5 "打つ" "ぶつ" "term" "[\"butsu definition 1\",\"butsu definition 2\"]" "vt" "P E1" 10 "Test Dictionary" 2
-          let res4 = Db.TermResult 6 "打つ" "ぶつ" "term" "[\"butsu definition 3\",\"butsu definition 4\"]" "vt" "P E2" 1 "Test Dictionary" 3
+          let res1 = Db.TermResult 3 "打つ" "うつ" "term" "[\"utsu definition 1\",\"utsu definition 2\"]" ["vt"] ["P", "E1"] ["v5"] 10 "Test Dictionary" 0
+          let res2 = Db.TermResult 4 "打つ" "うつ" "term" "[\"utsu definition 3\",\"utsu definition 4\"]" ["vt"] ["P", "E2"] ["v5"] 1 "Test Dictionary" 0
+          let res3 = Db.TermResult 5 "打つ" "ぶつ" "term" "[\"butsu definition 1\",\"butsu definition 2\"]" ["vt"] ["P", "E1"] ["v5"] 10 "Test Dictionary" 0
+          let res4 = Db.TermResult 6 "打つ" "ぶつ" "term" "[\"butsu definition 3\",\"butsu definition 4\"]" ["vt"] ["P", "E2"] ["v5"] 1 "Test Dictionary" 0
           results `shouldBe` [res1, res2, res3, res4]
         it "returns an empty list when disabling the dictionary" $ \conn -> do
           results <- Db.findTermsBulk conn ["打つ"] []
@@ -29,11 +29,14 @@ spec =
           results <- Db.findTermsBulk conn ["土木工事"] [1]
           map Db.termResultToJSON results
             `shouldBe` [ "{\"entryId\": 19,\
-                         \ \"expression\": \"土木工事\",\
+                         \ \"term\": \"土木工事\",\
                          \ \"reading\": \"どぼくこうじ\",\
-                         \ \"matchSource\": \"term\", \"glossary\": [\"dobokukouji definition\"],\
-                         \ \"definitionTags\": \"n\",\
-                         \ \"termTags\": \"\",\
+                         \ \"matchSource\": \"term\",\
+                         \ \"matchType\": \"exact\",\
+                         \ \"definitions\": [\"dobokukouji definition\"],\
+                         \ \"definitionTags\": [\"n\"],\
+                         \ \"termTags\": [],\
+                         \ \"rules\": [\"n\"],\
                          \ \"score\": 1,\
                          \ \"dictionary\": \"Test Dictionary\",\
                          \ \"index\": 0}"
