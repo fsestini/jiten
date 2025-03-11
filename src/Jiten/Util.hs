deabee8f80f5afaeb0a1f149316770e352d8434f
Filippo Sestini <sestini.filippo@gmail.com>
2025-03-11 23:58:14 +0000

    feat: add util `findJust`

--- src/Jiten/Util.hs
+++ src/Jiten/Util.hs
@@ -1,5 +1,6 @@
-module Jiten.Util (sformat) where
+module Jiten.Util (sformat, findJust) where
 
+import Data.Foldable (asum)
 import Data.Text (Text)
 import qualified Data.Text.Format as Format
 import qualified Data.Text.Format.Params as Format
@@ -7,3 +8,6 @@ import qualified Data.Text.Lazy as LT
 
 sformat :: (Format.Params ps) => Format.Format -> ps -> Text
 sformat fmt = LT.toStrict . Format.format fmt
+
+findJust :: (a -> Maybe b) -> [a] -> Maybe b
+findJust f xs = asum (map f xs)
