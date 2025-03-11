ab760569f1180c2877cdd84267d8b6674f2aeda5
Filippo Sestini <sestini.filippo@gmail.com>
2025-03-11 21:22:34 +0000

    feat: add util `sformat`

--- src/Jiten/Util.hs
+++ src/Jiten/Util.hs
@@ -1,1 +1,9 @@
-module Jiten.Util where
+module Jiten.Util (sformat) where
+
+import Data.Text (Text)
+import qualified Data.Text.Format as Format
+import qualified Data.Text.Format.Params as Format
+import qualified Data.Text.Lazy as LT
+
+sformat :: (Format.Params ps) => Format.Format -> ps -> Text
+sformat fmt = LT.toStrict . Format.format fmt
