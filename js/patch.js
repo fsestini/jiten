a1a6a5e2633f9453401e273a86b4a449308db009
Filippo Sestini <sestini.filippo@gmail.com>
2025-03-20 23:06:34 +0000

    chore: format js/patch.js

--- js/patch.js
+++ js/patch.js
@@ -1,15 +1,15 @@
 function Collator(locale, options) {
   return {
-    compare: function(a, b) {
-      const result = a.localeCompare(b, locale, options)
+    compare: function (a, b) {
+      const result = a.localeCompare(b, locale, options);
       if (result < 0) {
-        return -1
+        return -1;
       } else if (result > 0) {
-        return 1
+        return 1;
       } else {
-        return 0
+        return 0;
       }
-    }
+    },
   };
 }
 
@@ -20,67 +20,69 @@ class Performance {
 
   mark(markName, markOptions) {
     if (this.enabled) {
-      console.log(markName, markOptions)
+      console.log(markName, markOptions);
     }
   }
 
   measure(measureName, startOrMeasureOptions, endMark) {
     if (this.enabled) {
-      console.log(measureName, startOrMeasureOptions, endMark)
+      console.log(measureName, startOrMeasureOptions, endMark);
     }
   }
 
-  now() { }
+  now() {}
 }
 
-var performance = new Performance()
+var performance = new Performance();
 
 function mkOptions(dictionaries) {
-  var edm = new Map()
+  var edm = new Map();
   for (let i = 0; i < dictionaries.length; i++) {
     edm.set(dictionaries[i], {
       index: i,
-      alias: '',
+      alias: "",
       allowSecondarySearches: false,
       partsOfSpeechFilter: false,
       useDeinflections: false,
-    })
+    });
   }
   return {
-    matchType: 'exact',
+    matchType: "exact",
     deinflect: true,
     removeNonJapaneseCharacters: true,
     textReplacements: [null],
-    searchResolution: 'letter',
-    language: 'ja',
+    searchResolution: "letter",
+    language: "ja",
     enabledDictionaryMap: edm,
-    primaryReading: '',
+    primaryReading: "",
     excludeDictionaryDefinitions: null,
-  }
+  };
 }
 
-var Node = { ELEMENT_NODE: 1, TEXT_NODE: 3 }
+var Node = { ELEMENT_NODE: 1, TEXT_NODE: 3 };
 
 class DocumentShim {
-  constructor() { }
-  createElement(tag) { return new NodeBuilder(tag) }
+  constructor() {}
+  createElement(tag) {
+    return new NodeBuilder(tag);
+  }
   createTextNode(text) {
-    var n = new NodeBuilder(null)
-    n.textContent = text
-    return n
+    var n = new NodeBuilder(null);
+    n.textContent = text;
+    return n;
   }
 }
 
-var document = new DocumentShim()
+var document = new DocumentShim();
 
 function findTermsDOM(mode, text, options) {
-  const results = translator.findTerms(mode, text, options)
-  const entries = results.dictionaryEntries
-  const nodes = []
+  const results = translator.findTerms(mode, text, options);
+  const entries = results.dictionaryEntries;
+  const nodes = [];
   for (const entry of entries) {
     //TODO: pass dictionaryInfo
-    const node = displayGenerator.createTermEntry(entry, [])
-    nodes.push(node.toObject())
+    const node = displayGenerator.createTermEntry(entry, []);
+    nodes.push(node.toObject());
   }
-  return nodes
+  return nodes;
 }
