f5e2aae34cc615f968e70258843db28b84d47c5e
Filippo Sestini <sestini.filippo@gmail.com>
2025-03-21 19:21:17 +0000

    chore: format yomi.js

--- js/yomi.js
+++ js/yomi.js
@@ -1,37 +1,37 @@
-import { Translator } from './yomitan/js/language/translator.js'
-import { DisplayGenerator } from './yomitan/js/display/display-generator.js';
+import { Translator } from "./yomitan/js/language/translator.js";
+import { DisplayGenerator } from "./yomitan/js/display/display-generator.js";
 
 class DictionaryDatabase {
   findTermsBulk(termList, dictionaries, matchType) {
     // TODO: use other arguments
-    const resultJson = _findTermsBulk(JSON.stringify(termList))
-    const result = JSON.parse(resultJson)
-    return result
+    const resultJson = _findTermsBulk(JSON.stringify(termList));
+    const result = JSON.parse(resultJson);
+    return result;
   }
 
   findTermMetaBulk(termList, dictionaries) {
     // TODO: use other arguments
-    const resultJson = _findTermMetaBulk(JSON.stringify(termList))
-    const result = JSON.parse(resultJson)
-    return result
+    const resultJson = _findTermMetaBulk(JSON.stringify(termList));
+    const result = JSON.parse(resultJson);
+    return result;
   }
 }
 
-var db = new DictionaryDatabase()
+var db = new DictionaryDatabase();
 var translator = new Translator(db);
 translator.prepare();
 
 class NodeBuilder {
   constructor(name) {
-    this.name = name
-    this.queried = new Map()
-    this.dataset = {}
-    this.textContent = null
-    this.children = []
-    this.nodeType = name == null ? 3 : 1
+    this.name = name;
+    this.queried = new Map();
+    this.dataset = {};
+    this.textContent = null;
+    this.children = [];
+    this.nodeType = name == null ? 3 : 1;
   }
   appendChild(node) {
-    this.children.push(node)
+    this.children.push(node);
   }
   querySelector(selector) {
     // Check if the selector already exists in the map
@@ -45,28 +45,27 @@ class NodeBuilder {
     return selected;
   }
   toObject() {
-    var result = {}
-    result.name = this.name
-    result.dataset = this.dataset
-    result.textContent = this.textContent
-    result.children = this.children.map(n => n.toObject())
-    result.queried = []
+    var result = {};
+    result.name = this.name;
+    result.dataset = this.dataset;
+    result.textContent = this.textContent;
+    result.children = this.children.map((n) => n.toObject());
+    result.queried = [];
     for (const [k, v] of this.queried) {
-      result.queried.push({ selector: k, selected: v.toObject() })
+      result.queried.push({ selector: k, selected: v.toObject() });
     }
-    return result
+    return result;
   }
 }
 
 class HtmlTemplateShim {
-  constructor() {
-  }
+  constructor() {}
   instantiate(name) {
-    return new NodeBuilder('template:' + name)
+    return new NodeBuilder("template:" + name);
   }
-  loadFromFiles(files) { }
+  loadFromFiles(files) {}
 }
 
-const displayGenerator = new DisplayGenerator(null, null)
-displayGenerator._templates = new HtmlTemplateShim()
-displayGenerator.prepare()
+const displayGenerator = new DisplayGenerator(null, null);
+displayGenerator._templates = new HtmlTemplateShim();
+displayGenerator.prepare();
