2c816e52fbaaef090988e1ccfaafc7f47cedfa72
Filippo Sestini <sestini.filippo@gmail.com>
2025-03-22 23:03:52 +0000

    feat(server): add cors middleware

--- src/Jiten/Server.hs
+++ src/Jiten/Server.hs
@@ -1,5 +1,6 @@
 module Jiten.Server where
 
+import Network.Wai.Middleware.Cors (simpleCors)
 import Text.Blaze ((!))
 import qualified Text.Blaze.Html.Renderer.Text as Blaze
 import qualified Text.Blaze.Html5 as H
@@ -9,6 +10,7 @@ import qualified Web.Scotty as Scotty
 
 serve :: IO ()
 serve = scotty 3000 $ do
+  Scotty.middleware simpleCors
   Scotty.get "/" $ do
     Scotty.html . Blaze.renderHtml $ do
       H.h1 "Jiten"
