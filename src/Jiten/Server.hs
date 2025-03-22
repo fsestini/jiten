1e1ca4391c111bcb0842d037a681dce351fe754a
Filippo Sestini <sestini.filippo@gmail.com>
2025-03-22 23:02:02 +0000

    feat(server): implement barebones root page

--- src/Jiten/Server.hs
+++ src/Jiten/Server.hs
@@ -1,10 +1,18 @@
 module Jiten.Server where
 
+import Text.Blaze ((!))
+import qualified Text.Blaze.Html.Renderer.Text as Blaze
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
 import Web.Scotty (scotty)
 import qualified Web.Scotty as Scotty
 
 serve :: IO ()
 serve = scotty 3000 $ do
+  Scotty.get "/" $ do
+    Scotty.html . Blaze.renderHtml $ do
+      H.h1 "Jiten"
+      H.a ! A.href "/search" $ "Search"
   Scotty.get "/api/status" $ do
     Scotty.text "Ok."
 
