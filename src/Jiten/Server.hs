f054f8df2f953602c7dad330264d93ad88c2864e
Filippo Sestini <sestini.filippo@gmail.com>
2025-03-23 00:14:50 +0000

    feat(server): implement search page

--- src/Jiten/Server.hs
+++ src/Jiten/Server.hs
@@ -1,22 +1,63 @@
 module Jiten.Server where
 
+import qualified Database.SQLite.Simple as Sql
+import qualified Jiten.Database as Db
+import qualified Jiten.Yomichan.Core as Core
+import qualified Jiten.Yomichan.Search as Search
+import qualified Jiten.Yomichan.SearchPageTemplate as SearchPageTemplate
 import Network.Wai.Middleware.Cors (simpleCors)
+import qualified Paths_jiten
 import Text.Blaze ((!))
 import qualified Text.Blaze.Html.Renderer.Text as Blaze
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
-import Web.Scotty (scotty)
+import Web.Scotty (ActionM, scotty)
 import qualified Web.Scotty as Scotty
 
-serve :: IO ()
-serve = scotty 3000 $ do
+data ServerConfig = ServerConfig
+  { cfgPort :: !Int,
+    cfgYomiCtx :: !Core.YomiContext
+  }
+
+serveDataFile :: FilePath -> FilePath -> ActionM ()
+serveDataFile dir name = do
+  fp <-
+    Scotty.liftIO $
+      Paths_jiten.getDataFileName ("vendor/yomitan/ext/" <> dir <> "/" <> name)
+  Scotty.file fp
+
+serve :: ServerConfig -> IO ()
+serve cfg = scotty (cfgPort cfg) $ do
   Scotty.middleware simpleCors
   Scotty.get "/" $ do
     Scotty.html . Blaze.renderHtml $ do
       H.h1 "Jiten"
       H.a ! A.href "/search" $ "Search"
+  Scotty.get "/search" $ do
+    qMay <- Scotty.queryParamMaybe "query"
+    contents <-
+      case qMay of
+        Just q ->
+          Scotty.liftIO (Search.findTermsHTML (cfgYomiCtx cfg) Search.Simple q)
+        Nothing -> pure [H.text "No results"]
+    Scotty.html . Blaze.renderHtml . SearchPageTemplate.instantiate $
+      mconcat contents
   Scotty.get "/api/status" $ do
     Scotty.text "Ok."
+  Scotty.get "/css/:file" $ Scotty.pathParam "file" >>= serveDataFile "css"
+  Scotty.get "/images/:file" $
+    Scotty.pathParam "file" >>= serveDataFile "images"
 
 runServer :: IO ()
-runServer = pure ()
+runServer = do
+  Sql.withConnection "jiten.db" $ \conn -> do
+    Db.initDatabase conn
+    dicts <- Db.getDictionaries conn
+    Core.withYomitan conn (pure (map fst dicts)) $ \ctx -> do
+      Search.setOptions ctx (map snd dicts)
+      serve
+        ( ServerConfig
+            { cfgPort = 3000,
+              cfgYomiCtx = ctx
+            }
+        )
