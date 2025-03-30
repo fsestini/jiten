4f57af9075e4d05e0b0f5c96cc403977cd57e2a4
Filippo Sestini <sestini.filippo@gmail.com>
2025-03-30 00:37:31 +0000

    fix: js runtime crash on multithreaded server

--- src/Jiten/Server.hs
+++ src/Jiten/Server.hs
@@ -1,5 +1,9 @@
 module Jiten.Server where
 
+import Control.Concurrent (forkIO, forkOS, rtsSupportsBoundThreads)
+import Control.Concurrent.MVar (MVar)
+import qualified Control.Concurrent.MVar as MVar
+import Control.Monad (forever, void)
 import Data.Function ((&))
 import Data.List (isPrefixOf)
 import Data.Text (Text)
@@ -12,7 +16,7 @@ import qualified Jiten.Yomichan.SearchPageTemplate as SearchPageTemplate
 import Network.Wai.Middleware.Cors (simpleCors)
 import qualified Network.Wai.Middleware.Static as Static
 import qualified Paths_jiten
-import Text.Blaze ((!))
+import Text.Blaze (Markup, (!))
 import Text.Blaze.Html (Html)
 import qualified Text.Blaze.Html.Renderer.Text as Blaze
 import qualified Text.Blaze.Html5 as H
@@ -22,7 +26,7 @@ import qualified Web.Scotty as Scotty
 
 data ServerConfig = ServerConfig
   { cfgPort :: !Int,
-    cfgYomiCtx :: !Core.YomiContext,
+    cfgYomiVar :: MVar (Text, MVar [Markup]),
     cfgDataDir :: !FilePath
   }
 
@@ -37,15 +41,19 @@ serve cfg = scotty (cfgPort cfg) $ do
     queryParamMay <- Scotty.queryParamMaybe "query"
     (queryRow, contents) <-
       case queryParamMay of
-        Just queryParam -> do
-          (mkQueryContainer queryParam,) <$> findTermsHTML (cfgYomiCtx cfg) Search.Split queryParam
+        Just queryParam ->
+          (mkQueryContainer queryParam,) <$> findTermsHTML queryParam
         Nothing -> pure (mempty, [H.text "No results"])
     Scotty.html . Blaze.renderHtml $
       SearchPageTemplate.instantiate (mconcat contents) queryRow
   Scotty.get "/api/status" $ do
     Scotty.text "Ok."
   where
-    findTermsHTML ctx m q = Scotty.liftIO (Search.findTermsHTML ctx m q)
+    findTermsHTML :: Text -> Scotty.ActionM [Markup]
+    findTermsHTML q = Scotty.liftIO $ do
+      v <- MVar.newEmptyMVar
+      MVar.putMVar (cfgYomiVar cfg) (q, v)
+      MVar.readMVar v
     mkQueryContainer :: Text -> Html
     mkQueryContainer txt =
       txt
@@ -65,19 +73,39 @@ serve cfg = scotty (cfgPort cfg) $ do
         then Just (cfgDataDir cfg <> "/" <> "vendor/yomitan/ext/" <> s)
         else Nothing
 
-runServer :: IO ()
-runServer = do
-  dataDir <- Paths_jiten.getDataDir
+runYomi :: MVar (Text, MVar [Markup]) -> IO ()
+runYomi v =
   Sql.withConnection "jiten.db" $ \conn -> do
     Db.initDatabase conn
     dicts <- Db.getDictionaries conn
     Core.withYomitan conn $ \ctx -> do
       -- TODO: set frequency dictionary
       Search.setOptions ctx (map snd dicts) Nothing
-      serve
-        ( ServerConfig
-            { cfgPort = 3000,
-              cfgYomiCtx = ctx,
-              cfgDataDir = dataDir
-            }
-        )
+      forever $ do
+        (query, returnVar) <- MVar.takeMVar v
+        result <- Search.findTermsHTML ctx Search.Split query
+        MVar.putMVar returnVar result
+
+{-
+The quickjs runtime does not like to be called from an OS thread other than the
+one it was started in. To avoid crashing it, and still be able to call js
+functions within a multithreaded server, we spawn an instance of Yomitan in
+its own dedicated bound thread, and interact with it from the server's request
+handlers via MVars.
+
+See https://github.com/haskell-github-trust/quickjs-hs/blob/aa516edfe67611d1e5fb469cb77f102030d57351/src/Quickjs.hs#L589
+-}
+runServer :: IO ()
+runServer = do
+  dataDir <- Paths_jiten.getDataDir
+  yomiVar <- MVar.newEmptyMVar
+  if rtsSupportsBoundThreads
+    then void (forkOS (runYomi yomiVar))
+    else void (forkIO (runYomi yomiVar))
+  serve
+    ( ServerConfig
+        { cfgPort = 3000,
+          cfgYomiVar = yomiVar,
+          cfgDataDir = dataDir
+        }
+    )
