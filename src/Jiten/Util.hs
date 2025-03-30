355ee66fdb09e7e9b51479266bf0e38382ee6af0
Filippo Sestini <sestini.filippo@gmail.com>
2025-03-31 00:16:28 +0100

    chore: add haddock comments to util fns

--- src/Jiten/Util.hs
+++ src/Jiten/Util.hs
@@ -15,9 +15,15 @@ sformat fmt = LT.toStrict . Format.format fmt
 strFormat :: (Format.Params ps) => Format.Format -> ps -> String
 strFormat fmt = LT.unpack . Format.format fmt
 
+-- | Return the first 'Just' value obtained by applying the function @f@
+-- to elements of the list.
+-- If no element produces a 'Just' value, returns 'Nothing'.
 findJust :: (a -> Maybe b) -> [a] -> Maybe b
 findJust f xs = asum (map f xs)
 
+-- | Decode JSON text into a value of type @a@.
+-- Throws an error if decoding fails.
+-- Uses 'Data.Aeson.decodeStrictText' internally.
 decodeJSON :: (FromJSON a) => Text -> a
 decodeJSON =
   Maybe.fromMaybe (error "decodeJSON: failed to decode JSON")
