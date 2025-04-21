a3cea076c813e9287f993a4de3b056411ea9ca17
Filippo Sestini <sestini.filippo@gmail.com>
2025-04-22 00:17:17 +0200

    feat: update database tests

--- test/Jiten/DatabaseSpec.hs
+++ test/Jiten/DatabaseSpec.hs
@@ -2,6 +2,8 @@ module Jiten.DatabaseSpec where
 
 import qualified Database.SQLite.Simple as Sql
 import qualified Jiten.Database as Db
+import qualified Jiten.TestUtil as TestUtil
+import qualified Jiten.Util as Util
 import qualified Jiten.Yomichan.Dictionary as Yomi
 import Test.Hspec (Spec, around, describe, it, shouldBe)
 
@@ -13,57 +15,34 @@ spec =
         it "returns the correct list of dictionaries" $ \conn -> do
           dicts <- Db.getDictionaries conn
           dicts `shouldBe` [(1, "Test Dictionary")]
+      describe "getDictionarySummaries" $ do
+        it "returns the correct dictionary summaries" $ \conn -> do
+          summaries <- Db.getDictionarySummaries conn
+          let pp = unlines . map (TestUtil.pprintJson . Util.encodeStrict)
+          pure (TestUtil.mkGolden pp "getDictionarySummaries" summaries)
       describe "findTermsBulk" $ do
-        it "returns terms" $ \conn -> do
-          results <-
-            Db.findTermsBulk
-              conn
-              (Db.TermQuery ["打つ"] ["Test Dictionary"] "exact")
-          let res1 = Db.TermResult 3 "打つ" (Just "うつ") "term" "[\"utsu definition 1\",\"utsu definition 2\"]" ["vt"] ["P", "E1"] ["v5"] 10 "Test Dictionary" 0
-          let res2 = Db.TermResult 4 "打つ" (Just "うつ") "term" "[\"utsu definition 3\",\"utsu definition 4\"]" ["vt"] ["P", "E2"] ["v5"] 1 "Test Dictionary" 0
-          let res3 = Db.TermResult 5 "打つ" (Just "ぶつ") "term" "[\"butsu definition 1\",\"butsu definition 2\"]" ["vt"] ["P", "E1"] ["v5"] 10 "Test Dictionary" 0
-          let res4 = Db.TermResult 6 "打つ" (Just "ぶつ") "term" "[\"butsu definition 3\",\"butsu definition 4\"]" ["vt"] ["P", "E2"] ["v5"] 1 "Test Dictionary" 0
-          results `shouldBe` [res1, res2, res3, res4]
         it "returns an empty list when disabling the dictionary" $ \conn -> do
           results <- Db.findTermsBulk conn (Db.TermQuery ["打つ"] [] "exact")
           results `shouldBe` []
-      describe "termResultToJSON" $ do
-        it "renders JSON result correctly" $ \conn -> do
-          results <-
-            Db.findTermsBulk
-              conn
-              (Db.TermQuery ["土木工事"] ["Test Dictionary"] "exact")
-          map Db.toTextJSON results
-            `shouldBe` [ "{\"id\": 19,\
-                         \ \"term\": \"土木工事\",\
-                         \ \"reading\": \"どぼくこうじ\",\
-                         \ \"matchSource\": \"term\",\
-                         \ \"matchType\": \"exact\",\
-                         \ \"definitions\": [\"dobokukouji definition\"],\
-                         \ \"definitionTags\": [\"n\"],\
-                         \ \"termTags\": [],\
-                         \ \"rules\": [\"n\"],\
-                         \ \"score\": 1,\
-                         \ \"dictionary\": \"Test Dictionary\",\
-                         \ \"index\": 0}"
-                       ]
+        describe "and toTextJSON" $ do
+          it "render JSON results correctly" $ \conn -> do
+            let qs = ["打", "打ち込む", "お手前", "土木工事", "好き"]
+            results <- Db.findTermsBulk conn (Db.TermQuery qs [dict] "exact")
+            let pp = unlines . map (TestUtil.pprintJson . Db.toTextJSON)
+            pure (TestUtil.mkGolden pp "findTermsBulk_json" results)
+      describe "findTermMetaBulk" $ do
+        describe "and toTextJSON" $ do
+          it "render JSON results correctly" $ \conn -> do
+            let qs = ["打", "打ち込む", "お手前", "土木工事", "好き"]
+            results <- Db.findTermMetaBulk conn (Db.TermMetaQuery qs [dict])
+            let pp = unlines . map (TestUtil.pprintJson . Db.toTextJSON)
+            pure (TestUtil.mkGolden pp "findTermMetaBulk_json" results)
   where
-    -- describe "termMetaResultToJSON" $ do
-    --   it "renders JSON result correctly" $ \conn -> do
-    --     results <-
-    --       Db.findTermMetaBulk
-    --         conn
-    --         (Db.TermMetaQuery ["土木工事"] ["Test Dictionary"])
-    --     map Db.toTextJSON results
-    --       `shouldBe` [ "{\"index\": 0, \"term\": \"土木工事\", \"mode\": \"pitch\",\
-    --                    \ \"data\": {\"pitches\":[{\"devoice\":3,\"position\":4}],\"reading\":\"どぼくこうじ\"},\
-    --                    \ \"dictionary\": \"Test Dictionary\"}"
-    --                  ]
-
+    dict = "Test Dictionary"
     dictPath = "./test/valid-dictionary1.zip" :: FilePath
+
     withValidDictionary1 f =
       Sql.withConnection ":memory:" $ \conn -> do
         Db.initDatabase conn
-        dict <- Yomi.openArchiveFile dictPath
-        Db.insertDictionary conn dict
+        Yomi.openArchiveFile dictPath >>= Db.insertDictionary conn
         f conn
