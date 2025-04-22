a15c1f35d4d05b0f8d0309d0ab7a93ef762a50b2
Filippo Sestini <sestini.filippo@gmail.com>
2025-04-22 22:35:36 +0200

    fix(test): pprintJson: throw error on malformed json

--- test/Jiten/TestUtil.hs
+++ test/Jiten/TestUtil.hs
@@ -1,19 +1,23 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Jiten.TestUtil where
 
 import Data.Aeson (Value)
 import qualified Data.Aeson as A
 import qualified Data.Aeson.Encode.Pretty as A.Pretty
+import Data.Maybe (fromMaybe)
 import Data.Text (Text)
-import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.Encoding as LTE
 import Test.Hspec.Golden
 
 pprintJson :: Text -> String
-pprintJson json =
-  case A.decodeStrictText json :: Maybe Value of
-    Nothing -> T.unpack json
-    Just decoded -> LT.unpack . LTE.decodeUtf8 . A.Pretty.encodePretty $ decoded
+pprintJson =
+  LT.unpack
+    . LTE.decodeUtf8
+    . A.Pretty.encodePretty
+    . fromMaybe (error "pprintJson: failed to parse JSON")
+    . A.decodeStrictText @Value
 
 mkGolden :: (a -> String) -> String -> a -> Golden String
 mkGolden pp name actualOutput =
