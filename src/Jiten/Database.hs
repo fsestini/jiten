2bfa8c548dc59391adeb2712c5f026cfaa4f4453
Filippo Sestini <sestini.filippo@gmail.com>
2025-04-24 00:35:14 +0200

    tidy(db): qualify SQLite imports

--- src/Jiten/Database.hs
+++ src/Jiten/Database.hs
@@ -22,7 +22,15 @@ import qualified Data.Text as T
 import qualified Data.Text.Format as Format
 import qualified Data.Text.Lazy as LT
 import Data.Time.Clock (getCurrentTime)
-import Database.SQLite.Simple (Connection, Error (..), FromRow (..), Only (Only), Query (..), SQLError (sqlErrorDetails), ToRow (..), execute, execute_, field, lastInsertRowId, query, query_, sqlError, withTransaction)
+import Database.SQLite.Simple
+  ( Connection,
+    Error (..),
+    FromRow (..),
+    Only (Only),
+    Query (..),
+    ToRow (..),
+  )
+import qualified Database.SQLite.Simple as SQL
 import qualified Jiten.Util as Util
 import qualified Jiten.Yomichan.Dictionary as Yomichan
 import qualified Jiten.Yomichan.Summary as Yomichan
@@ -53,7 +61,7 @@ data KanjiRow = KanjiRow
   }
 
 instance FromRow HeadingRow where
-  fromRow = HeadingRow <$> field <*> field
+  fromRow = HeadingRow <$> SQL.field <*> SQL.field
 
 instance ToRow KanjiRow where
   toRow (KanjiRow {..}) =
@@ -70,16 +78,16 @@ instance ToRow KanjiRow where
 -- TODO: should we include termRuleIdentifiers?
 initDatabase :: Connection -> IO ()
 initDatabase conn = do
-  execute_ conn "CREATE TABLE IF NOT EXISTS heading(id INTEGER PRIMARY KEY,term VARCHAR NOT NULL,reading VARCHAR NULL,CONSTRAINT unique_heading UNIQUE (term,reading))"
-  execute_ conn dictionarySchema
-  execute_ conn "CREATE TABLE IF NOT EXISTS entry(id INTEGER PRIMARY KEY,glossary VARCHAR NOT NULL,definition_tags VARCHAR NOT NULL,term_tags VARCHAR NOT NULL,popularity INTEGER NOT NULL,rules VARCHAR NOT NULL,heading_id INTEGER NOT NULL REFERENCES heading ON DELETE RESTRICT ON UPDATE RESTRICT,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT)"
-  execute_ conn "CREATE TABLE IF NOT EXISTS tag(id INTEGER PRIMARY KEY,name VARCHAR NOT NULL,category VARCHAR NOT NULL,sorting_order INTEGER NOT NULL,notes VARCHAR NOT NULL,popularity INTEGER NOT NULL,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT,CONSTRAINT unique_tag_name UNIQUE (name,dictionary_id))"
-  execute_ conn "CREATE TABLE IF NOT EXISTS kanji(id INTEGER PRIMARY KEY,kanji VARCHAR NOT NULL,onyomi VARCHAR NOT NULL,kunyomi VARCHAR NOT NULL,tags VARCHAR NOT NULL,meanings VARCHAR NOT NULL,stats BLOB NOT NULL,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT,CONSTRAINT unique_kanji UNIQUE (kanji,dictionary_id))"
-  execute_ conn "CREATE TABLE IF NOT EXISTS kanji_frequency(id INTEGER PRIMARY KEY,kanji VARCHAR NOT NULL,data VARCHAR NOT NULL,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT,CONSTRAINT unique_kanji_frequency UNIQUE (kanji,data,dictionary_id))"
-  execute_ conn "CREATE TABLE IF NOT EXISTS media(id INTEGER PRIMARY KEY,path VARCHAR NOT NULL,content BLOB NOT NULL,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT,CONSTRAINT unique_path UNIQUE (path,dictionary_id))"
-  execute_ conn "CREATE TABLE IF NOT EXISTS term_meta(id INTEGER PRIMARY KEY,term VARCHAR NOT NULL,mode VARCHAR NOT NULL,data VARCHAR NOT NULL,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT,CONSTRAINT unique_term_meta UNIQUE (term,mode,data,dictionary_id))"
-  execute_ conn "CREATE INDEX IF NOT EXISTS heading_reading_ix ON heading(reading)"
-  execute_ conn "CREATE INDEX IF NOT EXISTS entry_heading_ix ON entry(heading_id)"
+  SQL.execute_ conn "CREATE TABLE IF NOT EXISTS heading(id INTEGER PRIMARY KEY,term VARCHAR NOT NULL,reading VARCHAR NULL,CONSTRAINT unique_heading UNIQUE (term,reading))"
+  SQL.execute_ conn dictionarySchema
+  SQL.execute_ conn "CREATE TABLE IF NOT EXISTS entry(id INTEGER PRIMARY KEY,glossary VARCHAR NOT NULL,definition_tags VARCHAR NOT NULL,term_tags VARCHAR NOT NULL,popularity INTEGER NOT NULL,rules VARCHAR NOT NULL,heading_id INTEGER NOT NULL REFERENCES heading ON DELETE RESTRICT ON UPDATE RESTRICT,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT)"
+  SQL.execute_ conn "CREATE TABLE IF NOT EXISTS tag(id INTEGER PRIMARY KEY,name VARCHAR NOT NULL,category VARCHAR NOT NULL,sorting_order INTEGER NOT NULL,notes VARCHAR NOT NULL,popularity INTEGER NOT NULL,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT,CONSTRAINT unique_tag_name UNIQUE (name,dictionary_id))"
+  SQL.execute_ conn "CREATE TABLE IF NOT EXISTS kanji(id INTEGER PRIMARY KEY,kanji VARCHAR NOT NULL,onyomi VARCHAR NOT NULL,kunyomi VARCHAR NOT NULL,tags VARCHAR NOT NULL,meanings VARCHAR NOT NULL,stats BLOB NOT NULL,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT,CONSTRAINT unique_kanji UNIQUE (kanji,dictionary_id))"
+  SQL.execute_ conn "CREATE TABLE IF NOT EXISTS kanji_frequency(id INTEGER PRIMARY KEY,kanji VARCHAR NOT NULL,data VARCHAR NOT NULL,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT,CONSTRAINT unique_kanji_frequency UNIQUE (kanji,data,dictionary_id))"
+  SQL.execute_ conn "CREATE TABLE IF NOT EXISTS media(id INTEGER PRIMARY KEY,path VARCHAR NOT NULL,content BLOB NOT NULL,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT,CONSTRAINT unique_path UNIQUE (path,dictionary_id))"
+  SQL.execute_ conn "CREATE TABLE IF NOT EXISTS term_meta(id INTEGER PRIMARY KEY,term VARCHAR NOT NULL,mode VARCHAR NOT NULL,data VARCHAR NOT NULL,dictionary_id INTEGER NOT NULL REFERENCES dictionary ON DELETE RESTRICT ON UPDATE RESTRICT,CONSTRAINT unique_term_meta UNIQUE (term,mode,data,dictionary_id))"
+  SQL.execute_ conn "CREATE INDEX IF NOT EXISTS heading_reading_ix ON heading(reading)"
+  SQL.execute_ conn "CREATE INDEX IF NOT EXISTS entry_heading_ix ON entry(heading_id)"
   where
     dictionarySchema =
       mconcat
@@ -101,7 +109,7 @@ initDatabase conn = do
 getDictionarySummaries :: Connection -> IO [Yomichan.Summary]
 getDictionarySummaries conn = do
   rs <-
-    query_ conn . mconcat $
+    SQL.query_ conn . mconcat $
       [ "SELECT name, terms_count, term_meta_count, tag_meta_count ",
         "FROM dictionary"
       ]
@@ -122,8 +130,8 @@ insertIndex :: Connection -> Yomichan.Index -> IO Int64
 insertIndex conn index = do
   now <- getCurrentTime
   let dictRow = (name, revision, sequenced, now)
-  execute conn cmd dictRow
-  lastInsertRowId conn
+  SQL.execute conn cmd dictRow
+  SQL.lastInsertRowId conn
   where
     name = Yomichan.indexTitle index
     revision = Yomichan.indexRevision index
@@ -142,7 +150,7 @@ updateDictionaryCounts conn dId = do
   termsC <- queryCount "entry"
   metaC <- queryCount "term_meta"
   tagC <- queryCount "tag"
-  execute conn updateStmt (termsC, metaC, tagC, dId)
+  SQL.execute conn updateStmt (termsC, metaC, tagC, dId)
   where
     queryCount :: Text -> IO Int
     queryCount table = do
@@ -150,7 +158,7 @@ updateDictionaryCounts conn dId = do
             Util.sformat
               "SELECT COUNT(*) FROM {} r WHERE r.dictionary_id = {}"
               (table, dId)
-      [Only c] <- query_ conn (Query q)
+      [Only c] <- SQL.query_ conn (Query q)
       pure c
     updateStmt =
       mconcat $
@@ -163,24 +171,24 @@ updateDictionaryCounts conn dId = do
 
 findDictionaryId :: Connection -> Text -> IO (Maybe DictionaryId)
 findDictionaryId conn dictName = do
-  query conn "SELECT id FROM dictionary WHERE name = ?" (Only dictName) >>= \case
+  SQL.query conn "SELECT id FROM dictionary WHERE name = ?" (Only dictName) >>= \case
     (Only dictId : _) -> pure (Just dictId)
     [] -> pure Nothing
 
 getDictionaries :: Connection -> IO [(DictionaryId, Text)]
-getDictionaries conn = query_ conn "SELECT id, name FROM dictionary"
+getDictionaries conn = SQL.query_ conn "SELECT id, name FROM dictionary"
 
 getOrInsertHeading :: Connection -> Text -> Maybe Text -> IO Int64
 getOrInsertHeading conn term reading = do
-  query conn "SELECT id FROM heading WHERE term = ? AND reading = ?" (term, reading) >>= \case
+  SQL.query conn "SELECT id FROM heading WHERE term = ? AND reading = ?" (term, reading) >>= \case
     (Only hId : _) -> pure hId
     [] -> do
-      execute conn "INSERT INTO heading (term, reading) VALUES (?,?)" (term, reading)
-      lastInsertRowId conn
+      SQL.execute conn "INSERT INTO heading (term, reading) VALUES (?,?)" (term, reading)
+      SQL.lastInsertRowId conn
 
 insertTag :: Connection -> DictionaryId -> Yomichan.Tag -> IO Int64
 insertTag conn dictId tag = do
-  execute
+  SQL.execute
     conn
     "INSERT INTO tag (name, category, sorting_order, notes, popularity, dictionary_id) VALUES (?,?,?,?,?,?)"
     ( Yomichan.tagName tag,
@@ -190,7 +198,7 @@ insertTag conn dictId tag = do
       Yomichan.tagScore tag,
       dictId
     )
-  lastInsertRowId conn
+  SQL.lastInsertRowId conn
 
 insertTerm :: Connection -> DictionaryId -> Yomichan.Term -> IO Int64
 insertTerm conn dictId term = do
@@ -199,7 +207,7 @@ insertTerm conn dictId term = do
       conn
       (Yomichan.termText term)
       (Yomichan.termReading term)
-  execute
+  SQL.execute
     conn
     "INSERT INTO entry (glossary, definition_tags, term_tags, rules, popularity, rules, heading_id, dictionary_id) VALUES (?,?,?,?,?,?,?,?)"
     ( LT.toStrict . A.encodeToLazyText $ Yomichan.termDefinitions term,
@@ -211,11 +219,11 @@ insertTerm conn dictId term = do
       hId,
       dictId
     )
-  lastInsertRowId conn
+  SQL.lastInsertRowId conn
 
 insertTermMeta :: Connection -> DictionaryId -> Yomichan.TermMeta -> IO ()
 insertTermMeta conn dictId termMeta = do
-  execute
+  SQL.execute
     conn
     "INSERT INTO term_meta (term, mode, data, dictionary_id) VALUES (?,?,?,?)"
     ( Yomichan.termMetaTerm termMeta,
@@ -224,8 +232,8 @@ insertTermMeta conn dictId termMeta = do
       dictId
     )
     `catch` \e ->
-      if sqlError e == ErrorConstraint
-        && T.isPrefixOf "UNIQUE constraint failed" (sqlErrorDetails e)
+      if SQL.sqlError e == ErrorConstraint
+        && T.isPrefixOf "UNIQUE constraint failed" (SQL.sqlErrorDetails e)
         then
           putStrLn
             ( Util.strFormat
@@ -236,7 +244,7 @@ insertTermMeta conn dictId termMeta = do
 
 insertKanji :: Connection -> DictionaryId -> Yomichan.Kanji -> IO ()
 insertKanji conn dictId kanji =
-  execute
+  SQL.execute
     conn
     "INSERT INTO kanji (kanji, onyomi, kunyomi, tags, meanings, stats, dictionary_id) VALUES (?,?,?,?,?,?,?)"
     ( KanjiRow
@@ -251,7 +259,7 @@ insertKanji conn dictId kanji =
 
 insertKanjiMeta :: Connection -> DictionaryId -> Yomichan.KanjiMeta -> IO ()
 insertKanjiMeta conn dictId kanjiMeta =
-  execute
+  SQL.execute
     conn
     "INSERT INTO kanji_frequency (kanji, data, dictionary_id) VALUES (?,?,?)"
     ( Yomichan.kanjiMetaKanji kanjiMeta,
@@ -261,7 +269,7 @@ insertKanjiMeta conn dictId kanjiMeta =
 
 insertMedia :: Connection -> DictionaryId -> FilePath -> ByteString -> IO ()
 insertMedia conn dictId path content =
-  execute
+  SQL.execute
     conn
     "INSERT INTO media (path, content, dictionary_id) VALUES (?,?,?)"
     (path, content, dictId)
@@ -271,7 +279,7 @@ insertDictionary conn dict = do
   let index = Yomichan.dictionaryIndex dict
   dictIdMay <- liftIO $ findDictionaryId conn (Yomichan.indexTitle index)
   case dictIdMay of
-    Nothing -> withTransaction conn $ do
+    Nothing -> SQL.withTransaction conn $ do
       dictId <- insertIndex conn index
       let runStream s f = Yomichan.runStream (s dict) (void . f conn dictId)
       runStream Yomichan.streamTerms insertTerm
@@ -341,7 +349,7 @@ findTermsBulk conn (TermQuery {..}) = do
                 " AND (heading.term = ? OR heading.reading = ?)"
               ]
        in do
-            rows <- query conn (Query sqlQuery) (text, text)
+            rows <- SQL.query conn (Query sqlQuery) (text, text)
             pure (map (i,text,) rows)
 
     mkResult i text (term, reading, entryId, glossary, defTags, termTags, rules, score, dictionary) =
@@ -411,7 +419,7 @@ findTermMetaBulk conn (TermMetaQuery {..}) = do
                 " AND (term_meta.term = ?)"
               ]
        in do
-            rs <- query conn (Query sqlQuery) (Only text)
+            rs <- SQL.query conn (Query sqlQuery) (Only text)
             pure $ map (i,) rs
     mkResult i (term, mode, data_, dictionary) = TermMetaResult i term mode data_ dictionary
 
@@ -473,7 +481,7 @@ findTagMetaBulk conn = fmap Maybe.catMaybes . mapM findTagMeta
                 "WHERE tag.name = ? AND dictionary.name = ? ",
                 "LIMIT 1"
               ]
-       in query conn (Query sql) (dqrQuery, dqrDictionary) <&> \case
+       in SQL.query conn (Query sql) (dqrQuery, dqrDictionary) <&> \case
             [(name, category, order, notes, score)] ->
               Just (Tag name category order notes score dqrDictionary)
             _ -> Nothing
